ee9651c901ec585c9f624204f4dfbe77fcd5e22f
EmpthyLL <javileh40@gmail.com>
2024-09-17 23:09:26 +0700

    KMT

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Home</title>
+    <title>My Blogs</title>
     <style>
         .box{
             border: 1px solid gray;
             border-radius: 10px;
             padding: 20px;
-            margin-bottom: 15px;
         }
         .contain{
             display: grid;
