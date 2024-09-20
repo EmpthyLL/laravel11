d09a36525e5c8306ba878340cf60cfe9a4d3fbc6
howard leslie <howard@adaremit.co.id>
2024-09-20 17:00:33 +0700

    Kambing.

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -5,7 +5,7 @@
       <div class="grid md:grid-cols-2 gap-4">
         @foreach ($blogs as $blog)
           <a href="{{ url('blog/'.$blog['blog_id']) }}" class="block">
-            <article class=" rounded-lg hover:shadow-lg p-4 shadow-md bg-gray-100">
+            <article class=" rounded-lg hover:shadow-lg p-4 shadow-md bg-gray-100 min-h-[380px]">
                 <h2 class="text-2xl flex gap-2 flex-wrap items-center font-bold tracking-tight text-gray-900">
                   <span>{{ $blog['title'] }}</span>
                   <span><svg class="text-gray-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-smile-plus"><path d="M22 11v1a10 10 0 1 1-9-10"/><path d="M8 14s1.5 2 4 2 4-2 4-2"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/><path d="M16 5h6"/><path d="M19 2v6"/></svg></span>
