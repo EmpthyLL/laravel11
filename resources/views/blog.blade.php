f0ca69058304319d85b74b47fdf524b63cb02f23
EmpthyLL <javileh40@gmail.com>
2024-10-12 08:54:04 +0700

    gddfgd

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -151,7 +151,7 @@
             @method('delete')
             @csrf
             <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" data-hs-overlay="#DeleteDialog">
-            Close
+            Cancel
             </button>
             <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-red-600 text-white hover:bg-red-700 focus:outline-none focus:bg-red-700 disabled:opacity-50 disabled:pointer-events-none">
             Delete Post
